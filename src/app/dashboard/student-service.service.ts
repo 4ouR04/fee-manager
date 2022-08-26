c2070c612e828b05d2ef64e01d9270b923137da4
4ouR04 <amosmwongelah@gmail.com>
2022-08-26 17:55:46 +0300

    Update header style

--- src/app/dashboard/student-service.service.ts
+++ src/app/dashboard/student-service.service.ts
@@ -10,12 +10,21 @@ export interface Student {
   providedIn: 'root'
 })
 export class StudentServiceService {
+  // Should be private
+  // Public for testing
   Students: Student[] = [
-    { Name: "Amos Mwongela", Gender: 'Male', Balance: 0 },
-    { Name: "Collins Mutunga", Gender: 'Male', Balance: 1 },
-    { Name: "Victor Ngane", Gender: 'Male', Balance: 2 },
-    { Name: "Movine Obae", Gender: 'Female', Balance: 4 },
+    { Name: "Amos", Gender: 'Male', Balance: 0 },
+    { Name: "Collins", Gender: 'Male', Balance: 1 },
+    { Name: "Victor", Gender: 'Male', Balance: 2 },
+    { Name: "Movine", Gender: 'Female', Balance: 4 },
     { Name: "Duke Angwenyi", Gender: 'Male', Balance: 7 },
+    { Name: "Alex ", Gender: "Male", Balance: 52},
+    { Name: "Breeeh", Gender: "Male", Balance: 12},
+    { Name: "John ", Gender: "Male", Balance: -12},
+    { Name: "Alex ", Gender: "Male", Balance: -10},
+    { Name: "Aicy", Gender: "Male", Balance: -9},
+    { Name: "Martin ", Gender: "Male", Balance: -1},
+    { Name: "Euphantus", Gender: "Male", Balance: -13}
 
    ]
 
